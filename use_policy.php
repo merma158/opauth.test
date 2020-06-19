debae9f2b96744a97280e0e12c193578d2241e4e
webdeveloper <javierurbano11@gmail.com>
2020-06-19 13:50:13 -0400

    Add use policy page

---
+++ use_policy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Condiciones de uso</title>
+</head>
+<body>
+  <h1>Condiciones de uso del sitio.</h1>
+  <p>
+    En éste sitio es con fin de realizar pruebas de delegación de autenticación Opauth.
+    No se publicará ni se expondrá el contenido y datos privados de quien utilice el método de autenticación, sus datos estarán asegurados por los proveedores de estratégia de autenticación y cuyos datos en éste sitio no podrán ser accedidos de ninguna manera.
+
+    Si acepta las políticas de éste sitio puede continuar su navegación.
+  </p>
+
+</body>
+</html>
